f309b58c0fc6e27249cbdc3e29e72460f1f4fa3b
Endre Czirbesz <endre.czirbesz@kodypay.com>
2022-09-13 14:14:44 +0100

    Fix SERVERURL env variable

--- ecs_service_wireguard.tf
+++ ecs_service_wireguard.tf
@@ -21,7 +21,7 @@ resource "aws_ecs_task_definition" "wireguard" {
           Value = var.server_tz
         },
         {
-          Name  = "SERVER_URL"
+          Name  = "SERVERURL"
           Value = var.server_url
         },
         {
